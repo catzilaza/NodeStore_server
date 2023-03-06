3b5e040c466fbd3d1ddc5bacb28f9d1fa4166d75
Ariya <catzilaza@gmail.com>
2023-03-07 02:18:59 +0700

    7/3/66

--- DataBase/connectDB.js
+++ DataBase/connectDB.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 function connectDb() {
   try {
     mongoose.set("strictQuery", true);
-    mongoose.connect('mongodb://127.0.0.1:27017/storesonline', {
+    mongoose.connect(process.env.URL_CLOUD_ATLAST+'storesDessert', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       //useCreateIndex: true,
